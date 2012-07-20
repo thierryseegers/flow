16f9e9b8d0bf7b7357e2fb200b94389616bfe5fb
Thierry Seegers <github@seegers.name>
2012-07-19 18:22:32 -0700

    Added more dummies. Added one more test.
    --HG--
    branch : [codebase] tests

---
+++ tests/dummies.h
@@ -0,0 +1,66 @@
+#if !defined(FUNCTIONAL_DUMMIES_H)
+	 #define FUNCTIONAL_DUMMIES_H
+
+#include "node.h"
+
+template<typename T>
+class dummy_producer : public flow::producer<T>
+{
+public:
+	dummy_producer(size_t ins = 0) : flow::node("dummy_producer"), flow::producer<T>("dummy_producer", ins)
+	{
+		int i = 22;
+	}
+
+	~dummy_producer()
+	{
+		int i = 22;
+	}
+
+	virtual void produce()
+	{
+		int i = 22;
+	}
+};
+
+template<typename P, typename C>
+class dummy_transformer : public flow::transformer<P, C>
+{
+public:
+	dummy_transformer(size_t ins = 0, size_t outs = 0) : flow::node("dummy_transformer"), flow::transformer<P, C>("dummy_transformer", ins, outs)
+	{
+		int i = 22;
+	}
+
+	~dummy_transformer()
+	{
+		int i = 22;
+	}
+
+	virtual void ready(size_t)
+	{
+		int i = 22;
+	}
+};
+
+template<typename T>
+class dummy_consumer : public flow::consumer<T>
+{
+public:
+	dummy_consumer(size_t outs = 0) : flow::node("dummy_consumer"), flow::consumer<T>("dummy_consumer", outs)
+	{
+		int i = 22;
+	}
+
+	~dummy_consumer()
+	{
+		int i = 22;
+	}
+
+	virtual void ready(size_t)
+	{
+		int i = 22;
+	}
+};
+
+#endif
--- tests/functional.cpp
+++ tests/functional.cpp
@@ -1,4 +1,4 @@
-#include "dummy_consumer.h"
+#include "dummies.h"
 
 #include "flow.h"
 
@@ -8,18 +8,47 @@ using namespace std;
 
 bool empty_graph()
 {
-	flow::graph g;
+	{
+		flow::graph g;
+
+		g.start();
+	}
 
 	return true;
 }
 
-bool graph_dummy_nodes()
+bool graph_dummy_nodes_unconnected()
 {
-	flow::graph g;
+	{
+		flow::graph g;
 
-	g.add(make_shared<dummy_consumer>());
+		g.add(make_shared<dummy_producer<int>>());
+		g.add(make_shared<dummy_transformer<int, int>>());
+		g.add(make_shared<dummy_consumer<int>>());
 
-	g.start();
+		g.start();
+	}
+
+	return true;
+}
+
+bool graph_dummy_nodes_connected()
+{
+	{
+		flow::graph g;
+
+		auto sp_dp = make_shared<dummy_producer<int>>(1);
+		auto sp_dt = make_shared<dummy_transformer<int, int>>(1, 1);
+		auto sp_dc = make_shared<dummy_consumer<int>>(1);
+		g.add(sp_dp);
+		g.add(sp_dt);
+		g.add(sp_dc);
+
+		g.connect<int>(sp_dp, 0, sp_dt, 0);
+		g.connect<int>(sp_dt, 0, sp_dc, 0);
+
+		g.start();
+	}
 
 	return true;
 }
@@ -32,9 +61,13 @@ int main(int argc, char* argv[])
 	{
 		b = empty_graph();
 	}
-	else if(strcmp(argv[1], "graph_dummy_nodes") == 0)
+	else if(strcmp(argv[1], "graph_dummy_nodes_unconnected") == 0)
 	{
-		b = graph_dummy_nodes();
+		b = graph_dummy_nodes_unconnected();
+	}
+	else if(strcmp(argv[1], "graph_dummy_nodes_connected") == 0)
+	{
+		b = graph_dummy_nodes_connected();
 	}
 
 	return b ? 0 : 1;
